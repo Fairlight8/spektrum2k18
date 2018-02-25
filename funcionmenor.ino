29966f9605b868a4aa2f082ad06637dd351f3dff
FairLight8 <marino@gamesfera.com>
2018-02-25 23:30:16 +0100

    funcion menor pseudocodigo

---
+++ funcionmenor.ino
@@ -0,0 +1,10 @@
+def funcionmenor{
+    //activotodoslosleds
+    //espero tiempo ultimo led
+    //apagoultimoled
+    //bajo el strobe
+    //espero 1000-tiempoultimoled-tiemporecuperacion
+    //leo r2d2
+    //subo strobe
+    //esperotiempo de rec
+}
